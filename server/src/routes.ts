5c56678e5ab0eba726bbfe237254c74d159ed03b
EduardoJM <eduardo_y05@outlook.com>
2020-06-05 11:39:53 -0300

    move validation to middlewares

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import multer from 'multer';
-import { celebrate, Joi } from 'celebrate';
 
 import multerConfig from './config/multer';
+import validation from './middlewares/validation';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -20,32 +20,7 @@ routes.get('/points/:id', pointsController.show);
 routes.post(
     '/points',
     upload.single('image'),
-    /*
-    Observation 1: The phone number is parsed as string here
-    and has an length of 11 because is in the format:
-    xxnnnnnaaaa:
-    xx -> DDD (2 digits in brazilian numbers)
-    nnnnn -> five first numbers (original 4 numbers and the added 9)
-    aaaa -> last four numbers
-     */
-    /*
-    Observation 2: The UF has changed from max(2) to length(2)
-    because all of UF in Brazil has two letters.
-     */
-    celebrate({
-        body: Joi.object().keys({
-            name: Joi.string().required(),
-            email: Joi.string().required().email(),
-            whatsapp: Joi.string().required().length(11),
-            latitude: Joi.number().required(),
-            longitude: Joi.number().required(),
-            city: Joi.string().required(),
-            uf: Joi.string().required().length(2),
-            items: Joi.string().required(),
-        }),
-    }, {
-        abortEarly: false,
-    }),
+    validation,
     pointsController.create
 );
 

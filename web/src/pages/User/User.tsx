8127e12e3bdf000bb1b9d8bf1678ceabc720cc17
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-07-18 15:33:47 -0300

    begin creation of User page

--- web/src/pages/User/User.tsx
+++ web/src/pages/User/User.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { useAuth } from '../../hooks';
-import { Page } from '../../styles';
+import { Page, TabContent } from '../../styles';
 import Header from '../../components/Header';
 import UserHeader from '../../components/UserHeader';
 
 const User: React.FC = () => {
     const { user } = useAuth();
-    
+
+    if (!user) {
+        return <></>;
+    }
     return (
         <Page>
             <Header />
             <UserHeader user={user} />
+
+            <TabContent>
+                <h2>Nome Aqui</h2>
+                <span>{user.email}</span>
+            </TabContent>
         </Page>
     );
 };

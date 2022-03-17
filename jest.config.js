6a5fd655e8920ae6e31a3ed8401400b460839bb3
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-03-17 20:28:53 -0300

    begin moving old project structure

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    clearMocks: true,
+    preset: 'ts-jest',
+    projects: [
+        '<rootDir>/packages/**/jest.config.js',
+    ],
+    testEnvironment: 'node',
+    testMatch: [
+        '*.spec.ts',
+        '*.spec.tsx',
+    ],
+};
